a9c251a39c0ef519f87d41fe07aa41cd05b5081e
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-12 16:14:44 -0400

    initial commit, wrote form label test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm'
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+// Arrange 
+// Act 
+// Assert
+test("form labels are written correctly and have form control (input has id attribute)",()=>{
+  const {getByLabelText} = render(<ContactForm/>);
+  getByLabelText(/first name/i);
+  getByLabelText(/last name/i);
+  getByLabelText(/email/i);
+  getByLabelText(/message/i);
+
+
+})
+
+// const {getByLabelText} = render(<ContactForm/>);
+// const firstNameLabel = getByLabelText(/first name/i);
+// const lastNameLabel = getByLabelText(/last name/i);
+// const emailLabel = getByLabelText(/email/i);
+// const messageLabel = getByLabelText(/message/i);
+
